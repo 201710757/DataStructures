5a5154505e946f35fc52ecd89ded4680b60bec05
201710757 <neti69@naver.com>
2020-09-03 23:03:48 +0900

    Initial Commit

---
+++ Data/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data
+//
+//  Created by 김지훈 on 2020/09/03.
+//  Copyright © 2020 김지훈. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
